82bd69d258fde89e18aa9369bf64a6ea182b0ae4
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-04-14 02:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
+const errorDiv = document.getElementById("error");
+const loadingDiv = document.getElementById("loading");
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +8,39 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.src = url;
+
+    img.onload = () => resolve(img); // success
+    img.onerror = () => reject(`Failed to load image: ${url}`); // error
+  });
+}
+
+function downloadImages() {
+  output.innerHTML = "";
+  errorDiv.innerHTML = "";
+
+  loadingDiv.innerHTML = "Loading...";
+
+  const promises = images.map((img) => downloadImage(img.url));
+
+  Promise.all(promises)
+    .then((imgs) => {
+      loadingDiv.innerHTML = "";
+
+      imgs.forEach((img) => {
+        output.appendChild(img);
+      });
+    })
+    .catch((error) => {
+      loadingDiv.innerHTML = "";
+
+      errorDiv.innerHTML = error;
+    });
+}
+
+btn.addEventListener("click", downloadImages);
